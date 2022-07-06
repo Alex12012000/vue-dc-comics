6e247761cfd21ea0c9b90ea8af8d293b936d2ef7
Alex12012000 <arcex.29@gmail.com>
2022-07-06 16:26:15 +0200

    Main Content Section

---
+++ src/components/MainContentSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="content">
+                <h3>Content goes here</h3>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script>
+export default {
+    name: "MainContentSection"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/common.scss";
+
+section {
+    background-color: #1c1c1c;
+    .content {
+        padding: 30px;
+        color: white;
+    }
+}
+</style>
